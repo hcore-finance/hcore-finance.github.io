183935fc51490871a5332c27a5987a495a6afbad
0hcore <72402182+0hcore@users.noreply.github.com>
2020-10-17 15:57:41 +0200

    Initial commit

---
+++ precache-manifest.4ec04cbafb4032c37bb5ada455c2b523.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62de13c5cbaa5c1dc88773106c4e019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6acf8a17189c7092a141",
+    "url": "/static/css/main.1b65018e.chunk.css"
+  },
+  {
+    "revision": "2f7187da21a3ebae03d8",
+    "url": "/static/js/0.4af1fc05.chunk.js"
+  },
+  {
+    "revision": "4f431729128cfabe7148",
+    "url": "/static/js/10.5ef9ddd0.chunk.js"
+  },
+  {
+    "revision": "ac00ba7cccf6b6c907ed",
+    "url": "/static/js/3.c196af17.chunk.js"
+  },
+  {
+    "revision": "ca5c4c691ea0cbca780abf4100c36ae5",
+    "url": "/static/js/3.c196af17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4951e4d05cc0bb23e8a3",
+    "url": "/static/js/4.29b9a7a7.chunk.js"
+  },
+  {
+    "revision": "7922688d575e038819c2",
+    "url": "/static/js/5.ac556fd4.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/static/js/5.ac556fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a5cd2e0e26da2c9b6b",
+    "url": "/static/js/6.2924b7c5.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.2924b7c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74343dbe5cbd0d6c7387",
+    "url": "/static/js/7.4e0cb68f.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/7.4e0cb68f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d6b28305ccd4454a36",
+    "url": "/static/js/8.3322a3fa.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.3322a3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5e12893b75fb552028",
+    "url": "/static/js/9.2a3ad10b.chunk.js"
+  },
+  {
+    "revision": "6acf8a17189c7092a141",
+    "url": "/static/js/main.2a87c78e.chunk.js"
+  },
+  {
+    "revision": "24f67483f6c00df7f8ae",
+    "url": "/static/js/runtime-main.14bd1b50.js"
+  },
+  {
+    "revision": "4b35995ac8e62452b2bf2c0af510f1ae",
+    "url": "/static/media/ethereum-blockchain.4b35995a.svg"
+  },
+  {
+    "revision": "d2bf909d858f0f9e715eb3634cfb8bee",
+    "url": "/static/media/hcore_logo.d2bf909d.svg"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
